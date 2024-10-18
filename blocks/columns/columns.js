972f68c2218911e072e8043cc6e9ca19b96a6207
Rahul <ramyarahul@kpmg.com>
2024-10-18 10:40:12 +0530

    committed CSS for columns component

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -4,6 +4,7 @@ export default function decorate(block) {
 
   // setup image columns
   [...block.children].forEach((row) => {
+    row.classList.add('column-container');
     [...row.children].forEach((col) => {
       const pic = col.querySelector('picture');
       if (pic) {
@@ -12,6 +13,8 @@ export default function decorate(block) {
           // picture is only content in column
           picWrapper.classList.add('columns-img-col');
         }
+      } else {
+        col.classList.add('columns-content-col');
       }
     });
   });
